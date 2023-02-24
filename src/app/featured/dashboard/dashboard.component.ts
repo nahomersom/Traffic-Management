d1667ee7fd2ca0ffbce6e507bfb4a6e356ff06f6
nahomersom <nahomersom@gmail.com>
2023-02-24 17:34:28 +0300

    feat : dashboard module and component added

--- src/app/featured/dashboard/dashboard.component.ts
+++ src/app/featured/dashboard/dashboard.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AccumulationChartComponent, AccumulationChart, IAccAnimationCompleteEventArgs, AccPoints, IAccTextRenderEventArgs, IAccLoadedEventArgs, AccumulationTheme } from '@syncfusion/ej2-angular-charts';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+export class DashboardComponent implements OnInit{
+  constructor() {
+    this.legendSettings = {
+   visible: true, position: 'Right' 
+   };
+ }
   ngOnInit(): void {
+    this.chartData = [
+      { zone: 'jimma' ,accident: 19 },
+      { zone: 'adama' ,accident: 60 },
+      { zone: 'borena' ,accident: 20 },
+      { zone: 'Zewaye' ,accident: 30 },
+
+
+
+  ];
+  this.primaryXAxis = {
+      valueType: 'Category'
+  };
   }
+  public data: Object[] = [{ x: "OverSpeed", y: 60 }, { x: "No belt", y: 40 },{ x: "Phone while driving", y: 60 }, { x: "Red light", y: 40 },{ x: "Drink drive", y: 60 }];
+  @ViewChild("pie")
+  public pie: AccumulationChartComponent | AccumulationChart;
+  public execute = false;
+  public count = 0;
+  public primaryXAxis: Object;
+  public chartData: Object[];
+  public legendSettings: Object;
+  palette = [
+    "#9EDDC0",
+    "#5CCC99",
+    "#1AA968",
+    "#068D45",
+    "#006F3D"
+  ];
+
+  public dataLabel: Object = {
+    visible: true,
+    position: "Inside",
+    name: "${point.y}",
+    font: {
+      color: "white",
+      fontWeight: "Bold",
+      size: "14px"
+    }
+  };
+
+  public resized(args): void {
+    this.pie.refresh();
+  }
+
 
 }
--- src/app/featured/dashboard/dashboard.module.ts
+++ src/app/featured/dashboard/dashboard.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
-
-
+const routes:Routes = [
+{
+  path:'',
+  component: DashboardComponent
+}
+];
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DashboardModule { }
