96bc446c13c580c1c43abf38111efcb802051632
nahomersom <nahomersom@gmail.com>
2023-02-24 09:11:01 +0300

    feat: project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TrafficManagement'`, () => {
+  it(`should have as title 'UnemployedAndEnterpriseManagement'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TrafficManagement');
+    expect(app.title).toEqual('UnemployedAndEnterpriseManagement');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TrafficManagement app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('UnemployedAndEnterpriseManagement app is running!');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Mgo+DSMBaFt/QHRqVVhkVFpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF5jS3xQdkRmWXxbc3xRRw==;Mgo+DSMBPh8sVXJ0S0J+XE9AflRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TdEdgWX5bcHdQQmBeUA==;ORg4AjUWIQA/Gnt2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkdhWn9fdXRWQ2RdVkU=;MTE4NTQ4MUAzMjMwMmUzNDJlMzBLeUJSL3hqL05FZ04wVGo5eE9FbTZWUC9UV3hwYm1ZT3pFbkMzeHY4NHFBPQ==;MTE4NTQ4MkAzMjMwMmUzNDJlMzBmVWlIWGRvWTFMZHBnT1hpeTNnSGFaOXNLWVUyYjQ1TzZ1amg2bTdVMVRBPQ==;NRAiBiAaIQQuGjN/V0Z+WE9EaFtKVmJLYVB3WmpQdldgdVRMZVVbQX9PIiBoS35RdUVhW3xecXFVRWVZUUF3;MTE4NTQ4NEAzMjMwMmUzNDJlMzBmUmgxSjJMd0x4Sng1OWpibnBFeS9SSkdqdHFLRUZqMGthZG1ISkVzV1JvPQ==;MTE4NTQ4NUAzMjMwMmUzNDJlMzBWY2RrSkVVbDdSdk9tRXZoVHV0YUIrakxrTVVQQ3kxeHEwV2xkODAzMlZ3PQ==;Mgo+DSMBMAY9C3t2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkdhWn9fdXRWQ2ZfVUU=;MTE4NTQ4N0AzMjMwMmUzNDJlMzBCbGl3R0FUdTBzZU9GVkxqaVk0Ti9TbTRpVXVmQklwM2FDNGM2dk5nQW9jPQ==;MTE4NTQ4OEAzMjMwMmUzNDJlMzBjTm45ckk3NDBXeW8wdWVka2c1dk03ZUpkczVTc0NaRXhXWkVsdnhnVmJ3PQ==;MTE4NTQ4OUAzMjMwMmUzNDJlMzBmUmgxSjJMd0x4Sng1OWpibnBFeS9SSkdqdHFLRUZqMGthZG1ISkVzV1JvPQ==');
 if (environment.production) {
   enableProdMode();
 }
